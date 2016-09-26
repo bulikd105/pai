42d5e6925b13ea22b23720f88b506c83e8c03c3f
bulikd105 <bulikd105@gmail.com>
2016-09-26 13:53:18 +0200

    Lab 1 - Czytanie nazw plików

--- lab1/src/lab1/Lab1.java
+++ lab1/src/lab1/Lab1.java
@@ -1,5 +1,37 @@
 package lab1;
 
-public class Lab1 {
+/* 
+ * Napisz program, ktory pobiera liste plikow z linii polecen i wyswietla liczbe wierszy kazdego z nich. 
+ * Program powinien utworzyc jeden watek dla kazdego z plikow i uzyc tych watkow do zliczenia liczby wierszy kazdego z plikow rownoczenie. 
+ * Utworz wersje programu, ktora odczytuje pliki nie jednoczesnie a sekwencyjnie. 
+ * Porownaj wydajnosc wielowatkowego i jednowatkowego programu uzywajac System.currentTimeMillis() do okreslenia czasu wykonania. 
+ * Porownania dokonaj dla dwoch, trzech i pieciu plikow.
+ */
 
+public class Lab1 
+{
+	public static void main(String[] args)
+	{
+		// Check for input with filenames
+		if (args.length > 0)
+		{
+			// Loop on each file name
+			for(String fileName : args)
+			{		
+				// Check if file is a "*.txt" file
+				if(fileName.substring(fileName.length(), 3).equals("txt"))
+				{
+					
+				}
+				else 
+				{
+					System.out.println("This file: " + fileName + "is not a *.txt file\n");
+				}
+			}
+		}
+		else
+		{
+			System.out.println("No files was given in command line\n");
+		}
+	}
 }
