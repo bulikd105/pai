64d34b8ad21d3fa633a4d0243656096cef10d41d
Damian <Damian@DESKTOP-UM09VT4>
2016-09-26 22:34:24 +0200

    Lab 2 - Initial commit

---
+++ lab2/src/lab2/Lab2.java
@@ -0,0 +1,32 @@
+package lab2;
+
+
+/* 
+ * @Autor 
+ * Damian Dworak
+ * 187677
+ * 
+ * @Description
+ * Napisz program laczacy sie ze strona podana jako argument wywolania programu 
+ * i wypisujacy wszystkie znalezione na niej linki i adresy email wykorzystujac 
+ * w tym celu wyrazenia regularne (pakiet java.util.regex). 
+ * Oprocz tego program ma zapisac do pliku wszystkie parametry polaczenia, 
+ * adres IP komputera na ktorym znajduje sie strona oraz naglowek strony (zawartosc sekcji <head>).
+ * 
+ */
+
+public class Lab2 
+{
+	public static void main(String[] args)
+	{
+		if(args.length == 1)
+		{
+			
+			
+		}
+		else
+		{
+			System.out.println("Wrong number of arguments.\n Please verify\n");
+		}
+	}
+}
