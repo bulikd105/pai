20bd0ade948f90b51b8d1bbbf1c9aef10a5a8848
bulikd105 <bulikd105@gmail.com>
2016-09-28 14:59:26 +0200

    Lab 5 - przygotowanie głównego menu

--- lab5/src/lab5/Simulation.java
+++ lab5/src/lab5/Simulation.java
@@ -15,7 +15,36 @@ public class Simulation
 {
 	public static void main(String[] args) 
 	{
-
+		if(args.length == 1)
+		{
+			switch(args[0])
+			{
+				case "starvation" 	: 	System.out.println("Wybrales opcje STARVATION");
+										starvation();
+										break;
+				case "livelock" 	: 	System.out.println("Wybrales opcje LIVELOCK");
+										livelock();
+										break;
+				case "deadlock" 	:	System.out.println("Wybrales opcje DEADLOCK");
+										deadlock();
+										break;
+				default 			: 	System.out.println("Zly argument, podaj jeden z tych:\n1 - starvation\n2 - livelock\n3 - deadlock\n");
+			}
+		}
 	}
 
+	private static void deadlock() 
+	{
+		
+	}
+
+	private static void livelock() 
+	{
+		
+	}
+
+	private static void starvation() 
+	{
+		
+	}
 }
