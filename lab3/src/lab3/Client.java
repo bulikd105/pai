3690dd97ba8833dbfe5243beb46a7c87a3bed531
bulikd105 <bulikd105@gmail.com>
2016-09-28 12:28:39 +0200

    Lab 3 - poprawki i łapanie błędów w kliencie

--- lab3/src/lab3/Client.java
+++ lab3/src/lab3/Client.java
@@ -1,9 +1,11 @@
 package lab3;
 
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.net.Socket;
+import java.net.UnknownHostException;
 
 public class Client 
 {
@@ -12,41 +14,60 @@ public class Client
 		String serverName = "127.0.0.1";
 		int port = 6080;
 		
+		Socket socket = null;
+		PrintWriter out = null;
+		BufferedReader in = null;
+		BufferedReader stdIn = null;
+		
+		String userInput;
+		
 		try
 		{
-			Socket socket = new Socket(serverName, port);
-			PrintWriter out;
-			BufferedReader in;
-			BufferedReader stdIn;
-			
+			socket = new Socket(serverName, port);
+			out = new PrintWriter(socket.getOutputStream(), true);
+			in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
 			
 			while(true)
 			{
-				out = new PrintWriter(socket.getOutputStream(), true);
-				in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+				
 				stdIn = new BufferedReader(new InputStreamReader(System.in));
-				
-				String answer = in.readLine();
-	 
-				
-				String userInput;
-
 				System.out.println("Welcome: " + in.readLine());
 				System.out.print("input: ");
+				
 				while ((userInput = stdIn.readLine()) != null) 
 				{
 					out.println(userInput);
 					System.out.println("echo: " + in.readLine());
 					System.out.print("input: ");
 				}
-
-
-			}
-			
+			}	
+		}
+		catch (UnknownHostException e)
+		{
+			System.out.println("Nie rozpoznaje tego adresu: " + serverName);
+			System.exit(1);
+		} 
+		catch (IOException e) 
+		{
+			System.out.println("Blad przy pobieraniu danych dla tego adresu: " + serverName);
+			System.exit(1);
 		}
 		catch(Exception e)
 		{
-			System.out.println(e);
+			System.out.println("Blad: " + e);
+			System.exit(1);
 		}
+		
+		try 
+		{
+			out.close();
+			in.close();
+			stdIn.close();
+			socket.close();
+		} 
+		catch (IOException e) 
+		{
+			System.out.println("Klient, problem przy zamykaniu bufferow");
+		}	
 	}
 }
