9b84930f181f252b576b0fc4dfc6af372b7ff993
bulikd105 <bulikd105@gmail.com>
2016-09-28 14:53:03 +0200

    Lab 4 - initial commit

---
+++ lab4/src/lab4/Bank.java
@@ -0,0 +1,22 @@
+package lab4;
+
+/* 
+ * @Autor 
+ * Damian Dworak
+ * 187677
+ * 
+ * @Description
+ * Aplikacja obslugujaca bank czasu. 
+ * Klient zglasza (i wycofuje) uslugi jakie moze wykonac i ich terminy, 
+ * moze tez zarzadac wyswietlenia wszystkich dostepnych uslug w banku i zarezerwowac sobie wybrana usluge. 
+ * Serwer rozsyla komunikaty o nowych, zarezerwowanych, niewykorzystanych i wycofanych uslugach i terminach do wszystkich klientow. 
+ * Nalezy zadbac o odpowiednia synchronizacje dostepu do zasobow i ich aktualizacje.
+ */
+
+public class Bank 
+{
+	public static void main(String[] args)
+	{
+		
+	}
+}
