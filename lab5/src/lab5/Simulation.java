0d33a5e4435a2208163dabf29aced467f150b341
bulikd105 <bulikd105@gmail.com>
2016-09-28 14:53:13 +0200

    Lab 5 - initial commit

---
+++ lab5/src/lab5/Simulation.java
@@ -0,0 +1,21 @@
+package lab5;
+
+/* 
+ * @Autor 
+ * Damian Dworak
+ * 187677
+ * 
+ * @Description
+ * Napisz program, ktory zasumuluje nastepujace zjawiska: deadlock, livelock, starvation. 
+ * Rodzaj symulacji powinien byc parametrem wywolania programu. 
+ * Zrob ograniczenie czasowe na dane zjawisko, tak aby program sie nie zawiesil.
+ */
+
+public class Simulation 
+{
+	public static void main(String[] args) 
+	{
+
+	}
+
+}
