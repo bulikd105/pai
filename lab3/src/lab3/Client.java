47238f009b848b716cad780c753e081d05fccd49
bulikd105 <bulikd105@gmail.com>
2016-09-28 12:10:37 +0200

    Lab 3 - dodanie przechwytywania informacji w kliencie

--- lab3/src/lab3/Client.java
+++ lab3/src/lab3/Client.java
@@ -1,19 +1,31 @@
 package lab3;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
+
 public class Client 
 {
 	public static void main(String[] args)
 	{
-		String serverName = "";
+		String serverName = "127.0.0.1";
 		int port = 6080;
 		
 		try
 		{
+			Socket socket = new Socket(serverName, port);
 			
+			PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
+			BufferedReader input = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+			String answer = input.readLine();
+ 
+			System.out.println(answer);
+ 
 		}
 		catch(Exception e)
 		{
-			
+			System.out.println(e);
 		}
 	}
 }
