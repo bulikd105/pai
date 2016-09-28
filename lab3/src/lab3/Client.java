4488331dff07c23f4d739f26ece5db57f46470f7
bulikd105 <bulikd105@gmail.com>
2016-09-28 12:03:05 +0200

    Lab 3 - przygotowanie klienta

--- lab3/src/lab3/Client.java
+++ lab3/src/lab3/Client.java
@@ -2,5 +2,18 @@ package lab3;
 
 public class Client 
 {
-
+	public static void main(String[] args)
+	{
+		String serverName = "";
+		int port = 6080;
+		
+		try
+		{
+			
+		}
+		catch(Exception e)
+		{
+			
+		}
+	}
 }
