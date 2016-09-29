21d93feaa48a4cdb09a7fe9950bbe26e7a15aa1b
bulikd105 <bulikd105@gmail.com>
2016-09-29 08:53:47 +0200

    Lab 5 - starvation, przygotowanie konstruktora, oraz deklaracja metody

--- lab5/src/lab5/Starvation.java
+++ lab5/src/lab5/Starvation.java
@@ -2,5 +2,18 @@ package lab5;
 
 public class Starvation 
 {
+	private String name;
+	public int count;
+	
+	public Starvation(String name)
+	{
+		this.name = name;
+		this.count = 0;
+	}
+	
+	public void Distance()
+	{
+		
+	}
 
 }
