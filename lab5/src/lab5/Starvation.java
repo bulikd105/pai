339dd76be08affb0bda561dafbde45f50ad465e7
bulikd105 <bulikd105@gmail.com>
2016-09-29 08:57:37 +0200

    Lab 5 - dodanie setera dla pola isActive

--- lab5/src/lab5/Starvation.java
+++ lab5/src/lab5/Starvation.java
@@ -2,18 +2,27 @@ package lab5;
 
 public class Starvation 
 {
+	private boolean isActive;
 	private String name;
-	public int count;
+	private int count;
 	
 	public Starvation(String name)
 	{
 		this.name = name;
+		this.isActive = true;
 		this.count = 0;
 	}
 	
-	public void Distance()
+	public void setActive(boolean isActive) 
 	{
-		
+		this.isActive = isActive;
 	}
 
+	public void Distance()
+	{
+		while(isActive)
+		{
+			
+		}
+	}
 }
