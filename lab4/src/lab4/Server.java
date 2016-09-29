09cfce11a50eb8f469bbd2aa79e42a9ef2cc5c9b
bulikd105 <bulikd105@gmail.com>
2016-09-29 11:40:48 +0200

    Lab 4 - reuse klasy server z zadania 3

--- lab4/src/lab4/Server.java
+++ lab4/src/lab4/Server.java
@@ -1,5 +1,9 @@
 package lab4;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
 import java.net.Socket;
 
 public class Server implements Runnable 
@@ -17,8 +21,38 @@ public class Server implements Runnable
 	@Override
 	public void run() 
 	{
-		// TODO Auto-generated method stub
-		
+		try
+		{
+			// Bufery do odbierania i wysylania wiadomosci
+            BufferedReader in = new BufferedReader( new InputStreamReader(socket.getInputStream()));
+            PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
+            String userInput = "";
+            
+            // Uruchomienie glownej petli
+            while((userInput = in.readLine()) != null)
+            {
+            	
+            }
+            
+        	// Zamkniecie bufferow
+        	in.close();
+        	out.close();
+		}
+		catch(IOException e)
+		{
+			System.out.println("Klient " + clientNumber + " zglosil blad: " + e);
+		}
+		finally // W razie gdyby klient zglosil blad, rozlacz go
+		{
+			try
+			{
+				System.out.println("Odlaczanie klienta nr: " + clientNumber);
+				socket.close();
+			}
+			catch (IOException e) 
+			{
+				 System.out.println("Odlaczanie klienta nr: " + clientNumber + " nie powiodlo sie");
+			}
+		}
 	}
-
 }
