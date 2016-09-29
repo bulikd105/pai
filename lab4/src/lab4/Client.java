2ae4c6779837b9deebc0240308228e56685a74cc
bulikd105 <bulikd105@gmail.com>
2016-09-29 11:47:41 +0200

    Lab 4 - reuse klasy Client z zadania 3

---
+++ lab4/src/lab4/Client.java
@@ -0,0 +1,94 @@
+package lab4;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
+
+public class Client 
+{
+	public static void main(String[] args) 
+	{
+		// Parametry serwera
+		String serverName = "127.0.0.1";
+		int port = 6080;
+		
+		// Wstepna deklaracja bufferow
+		Socket socket = null;
+		PrintWriter out = null;
+		BufferedReader in = null;
+		BufferedReader stdIn = null;
+
+		
+		// Polaczenie sie do serwera
+		try
+		{
+			socket = new Socket(serverName, port);	
+		}
+		catch(IOException e) 
+		{
+			System.out.println("BLAD: Nie mozna sie polaczyc z tym serwerem: " + serverName);
+			System.exit(1);
+		}
+		
+		// Dzialania na serwerze
+		try 
+		{	
+			// Zmienne do przechwytywania odpowiedzi
+			String userInput;
+			String serverAnswer;
+			
+			// Inicjalizacja bufferow
+			out = new PrintWriter(socket.getOutputStream(), true);
+			in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+			stdIn = new BufferedReader(new InputStreamReader(System.in));
+/*			
+			System.out.print("Klient: ");
+			System.out.println(in.readLine());*/
+			
+			// Pobieramy wiadomosc od usera
+			while ((userInput = stdIn.readLine()) != null) 
+			{
+/*				in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+				
+				// Wysylamy na serwer
+				out.println(userInput);
+				out.flush();
+				
+				serverAnswer = in.readLine();
+				
+				if(serverAnswer.equals("disconnect"))
+				{
+					System.out.println("Klient: zamykanie polaczenia");
+					socket.close();
+					break;
+				}
+
+				// Wyswietlamy odpowiedz servera
+				System.out.println("Server: " + serverAnswer);
+				
+				System.out.print("Klient: ");*/
+			}
+			
+			// Zamykanie bufferow
+			out.close();
+			in.close();
+			stdIn.close();
+		} 
+		catch (IOException e) 
+		{
+			System.out.println("BLAD: nie mozna pobrac danych z serwera" + e);
+			try 
+			{
+				System.out.println("Zamykanie polaczenia z serwerem");
+				socket.close();
+			} 
+			catch (IOException e1) 
+			{
+				System.out.println("BLAD: odlaczanie od serwera nie powiodlo sie");
+				System.exit(1);
+			}	
+		}	
+	}
+}
