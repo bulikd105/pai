b56c5ef0a386ada472cdcbc80d7de52d4cb8bd41
bulikd105 <bulikd105@gmail.com>
2016-09-29 10:32:50 +0200

    Lab 5 - Livelock, dodanie podklasy Thread, dla klasy Livelock

--- lab5/src/lab5/Livelock.java
+++ lab5/src/lab5/Livelock.java
@@ -1,6 +1,6 @@
 package lab5;
 
-public class Livelock implements Runnable
+public class Livelock 
 {
 	String person1;
 	String person2;
@@ -11,16 +11,32 @@ public class Livelock implements Runnable
 		this.person2 = person2;
 	}
 	
-	public void Hello(String person)
+	public void Hello(Livelock lock2)
 	{
 		
 	}
 	
-	@Override
-	public void run() 
+	public class Thread implements Runnable
 	{
 		
+		Livelock lock1;
+		Livelock lock2;
 		
+		void setLocker1(Livelock lock)
+		{
+			this.lock1 = lock;
+		}
+		
+		void setLocker2(Livelock lock)
+		{
+			this.lock2 = lock;
+		}
+
+		@Override
+		public void run() 
+		{
+			lock1.Hello(lock2);
+		}
 	}
 	
 }
